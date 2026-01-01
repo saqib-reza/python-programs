fbfd1f6af3ab314b48d45f90a5fdd85686d3d19f
saqib-reza <saqib.reza11@gmail.com>
2026-01-01 23:30:31 +0530

    Add bagels.py

---
+++ bagels.py
@@ -0,0 +1,15 @@
+import random
+NUM_DIGITS = 3  # (!) Try setting this to 1 or 10.
+MAX_GUESSES = 10  # (!) Try setting this to 1 or 100.
+
+def main():
+    print('''Bagels, a deductive logic game.
+By Al Sweigart al@inventwithpython.com
+I am thinking of a {}-digit number with no repeated digits.
+Try to guess what it is. Here are some clues:
+When I say:    That means:
+Pico         One digit is correct but in the wrong position.
+Fermi        One digit is correct and in the right position.
+Bagels       No digit is correct.
+For example, if the secret number was 248 and your guess was 843, the
+clues would be Fermi Pico.'''.format(NUM_DIGITS))
